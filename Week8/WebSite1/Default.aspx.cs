040574d5d7768a54fa1b6ed8cd53e053b94e9021
Saswat Levin <saswatlevin@gmail.com>
2019-10-01 20:44:27 +0530

    almost solved first question of week1

--- Week8/WebSite1/Default.aspx.cs
+++ Week8/WebSite1/Default.aspx.cs
@@ -11,30 +11,31 @@ public partial class _Default : System.Web.UI.Page
 {
     DataSet ds = new DataSet();
     string connectionString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=master;Integrated Security=True";
-    
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
-        string query = "SELECT staffid,firstname,lastname,dno,street,city,zipcode FROM dbo.house";
+        string query = "SELECT staffid FROM dbo.house";
         if (!IsPostBack)
         {
             LBNotify.Items.Add("Delhi");
             LBNotify.Items.Add("Chennai");
             LBNotify.Items.Add("Chandigarh");
 
-            try {
+            try
+            {
                 using (SqlConnection connection = new SqlConnection(connectionString))
-                {   
+                {
                     using (SqlCommand cmd = new SqlCommand(query, connection))
                     {
                         connection.Open();
-                        using (SqlDataAdapter adapter = new SqlDataAdapter(query,connection))
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
                         {
                             adapter.Fill(ds);
                         }
                     }
                 }
-               
+
             }
             catch (Exception ex)
             {
@@ -68,9 +69,10 @@ public partial class _Default : System.Web.UI.Page
     protected void DDLStaffId_SelectedIndexChanged(object sender, EventArgs e)
     {
 
+        //lbl_Display.Text += LBNotify.SelectedItem.Value.ToString();
         DataSet ds = new DataSet();
         string connectionString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=master;Integrated Security=True";
-        
+
         try
         {
             string query = "SELECT staffid FROM dbo.house";
@@ -98,13 +100,14 @@ public partial class _Default : System.Web.UI.Page
             DDLStaffId.DataTextField = "staffid";
             DDLStaffId.DataValueField = "staffid";
             DDLStaffId.DataBind();
-            /*LBNotify.DataSource = ds;
+            /*
+            LBNotify.DataSource = ds;
             LBNotify.DataTextField = "city";
             LBNotify.DataValueField = "city";
             LBNotify.DataBind();*/
 
 
-
+       
         }
 
         catch (Exception ex)
@@ -127,22 +130,33 @@ public partial class _Default : System.Web.UI.Page
             {
                 Response.Write("\nDataRow Error in foreach: " + ex);
             }
-        
+
     }
 
     protected void Button1_Click(object sender, EventArgs e)
-    {   
+    {
+
         using (SqlConnection connection = new SqlConnection(connectionString))
         {
+            int staffid = Convert.ToInt32(DDLStaffId.SelectedItem.Value);
+            string city = LBNotify.SelectedItem.Value.ToString();
             string query_btn = "UPDATE dbo.house SET city=@city WHERE staffid=@staffid";
             using (SqlCommand cmd = new SqlCommand(query_btn, connection))
             {
+               
+                cmd.Parameters.AddWithValue("@city", city);
+                cmd.Parameters.AddWithValue("@staffid",staffid);
                 connection.Open();
-                cmd.Parameters.AddWithValue("@city", LBNotify.SelectedIndex);
-                cmd.Parameters.AddWithValue("@staffid", DDLStaffId.SelectedIndex);
+                int result=cmd.ExecuteNonQuery();
+
+                //Response.Write("\nCity=" + city);
+                //Response.Write("\nStaffid="+staffid);
+                Response.Write("\nResult="+result);
+
             }
         }
 
     }
-        
+    
 }
+
