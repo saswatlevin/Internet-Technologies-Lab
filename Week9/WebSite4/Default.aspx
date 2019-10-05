07a96fb59d583b5f959e2d70a2f0bf29be179b96
Saswat Levin <saswatlevin@gmail.com>
2019-10-05 10:29:51 +0530

    Almost added Week9 Completely

---
+++ Week9/WebSite4/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lbl_category" Text="Category" runat="server" />
+        <asp:DropDownList ID="DDLCategory" runat="server" OnSelectedIndexChanged="DDLCategory_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList>
+         <asp:Label ID="lbl_names" Text="Name" runat="server" />
+         <asp:ListBox ID="LBNames" runat="server" OnSelectedIndexChanged="LBNames_SelectedIndexChanged" AutoPostBack="true"></asp:ListBox>
+        <br />
+        <br />
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString='<%$ ConnectionStrings:masterConnectionString %>' SelectCommand="SELECT * FROM [actor] WHERE ([name] = @name)">
+            <SelectParameters>
+                <asp:ControlParameter ControlID="LBNames" PropertyName="SelectedValue" Name="name" Type="String"></asp:ControlParameter>
+            </SelectParameters>
+        </asp:SqlDataSource>
+         <asp:Label ID="lbl_details" Text="Details" runat="server" />
+
+        <asp:GridView ID="GridView1" runat="server" DataSourceID="SqlDataSource1"></asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Week9/WebSite4/Default.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    List<string> category = new List<string>();
+    string connectionString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=master;Integrated Security=True";
+    DataSet ds = new DataSet();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            category.Add("Animated");
+            category.Add("Comedy");
+            category.Add("Romance");
+
+            DDLCategory.DataSource = category;
+            this.DataBind();
+
+           
+
+            
+        }
+    }
+
+    protected void DDLCategory_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT name FROM dbo.actor WHERE id=@id";
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@id", DDLCategory.SelectedIndex);
+                    connection.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string name = (string)reader["name"];
+                            LBNames.Items.Add(name);
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Response.Write("\n Error " + ex);
+        }
+    }
+    protected void LBNames_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
